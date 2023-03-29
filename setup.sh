b8940034dc23a8a196eef795faa454aebb8696a5
b.specht <bernhard.specht@pentlandfirth.com>
2023-03-29 13:18:51 +0200

    add sshrc

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ stow xdg-config
 stow local
 stow profile
 stow cargo
+stow ssh
 
 update-desktop-database ~/.local/share/applications/
 xdg-mime default org-protocol.desktop x-scheme-handler/org-protocol
